95e6d85365c3dadaf2d7f7c116bc178a1d7b92f9
ritikdevelop <hrithikrana216@gmail.com>
2024-12-31 01:13:03 +0530

    Implement user profile management with editable fields and state management

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,11 +1,155 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../assets/assets_frontend/assets";
 
 const MyProfile = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [userData, setUserData] = useState({
+    name: "John Doe",
+    image: assets.profile_pic,
+    email: "john.doe@example.com",
+    phone: "1234567890",
+    address: {
+      line1: "123 Main St",
+      line2: "Apt 101, New York",
+    },
+    gender: "Male",
+    dob: "1990-01-01",
+  });
 
-export default MyProfile
+  const [isEdit, setIsEdit] = useState(false);
+
+  return (
+    <div className="max-w-lg flex flex-col gap-2 text-sm">
+      <img className="w-36 rounded-full" src={userData.image} alt="UserImage" />
+
+      {isEdit ? (
+        <input
+          className="bg-gray-50 text-3xl font-medium max-w-60 mt-4"
+          type="text"
+          onChange={(e) =>
+            setUserData((prev) => ({ ...prev, name: e.target.value }))
+          }
+          value={userData.name}
+        />
+      ) : (
+        <p className="font-medium text-3xl text-neutral-800 mt-4">
+          {userData.name}
+        </p>
+      )}
+
+      <hr className="bg-zinc-400 h-[1px] border-none" />
+      <div>
+        <p className="text-neutral-500 underline mt-3">CONTACT INFORMATION</p>
+        <di className="grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700">
+          <p className="font-medium">Email id:</p>
+          <p className="text-blue-500">{userData.email}</p>
+          <p className="font-medium">Phone:</p>
+          {isEdit ? (
+            <input
+              className="bg-gray-100 max-w-52"
+              type="text"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, phone: e.target.value }))
+              }
+              value={userData.phone}
+            />
+          ) : (
+            <p className="text-blue-400">{userData.phone}</p>
+          )}
+          <p className="font-medium">Address:</p>
+          {isEdit ? (
+            <p>
+              <input
+                className="bg-gray-50"
+                onChange={(e) =>
+                  setUserData((prev) => ({
+                    ...prev,
+                    address: {
+                      ...prev.address,
+                      line1: e.target.value,
+                    },
+                  }))
+                }
+                value={userData.address.line1}
+                type="text"
+              />
+              <br />
+              <input
+                className="bg-gray-50"
+                onChange={(e) =>
+                  setUserData((prev) => ({
+                    ...prev,
+                    address: {
+                      ...prev.address,
+                      line2: e.target.value,
+                    },
+                  }))
+                }
+                value={userData.address.line2}
+                type="text"
+              />
+            </p>
+          ) : (
+            <p className="text-gray-500">
+              {userData.address.line1}
+              <br />
+              {userData.address.line2}
+            </p>
+          )}
+        </di>
+      </div>
+
+      <div>
+        <p className="text-neutral-500 underline mt-3">BASIC INFORMATION</p>
+        <div className="grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700">
+          <p className="font-medium">Gender:</p>
+          {isEdit ? (
+            <select
+              className="max-w-20 bg-gray-100"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, gender: e.target.value }))
+              }
+              value={userData.gender}
+            >
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          ) : (
+            <p className="text-gray-400">{userData.gender}</p>
+          )}
+          <p className="font-medium">Birthday:</p>
+          {isEdit ? (
+            <input
+              className="max-w-28 bg-gray-100"
+              type="date"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, dob: e.target.value }))
+              }
+              value={userData.dob}
+            />
+          ) : (
+            <p className="text-gray-400">{userData.dob}</p>
+          )}
+        </div>
+      </div>
+      <div className="mt-10">
+        {isEdit ? (
+          <button
+            className="border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all"
+            onClick={() => setIsEdit(false)}
+          >
+            Save information
+          </button>
+        ) : (
+          <button
+            className="border border-primary px-8 py-2 rounded-full  hover:bg-primary hover:text-white transition-all"
+            onClick={() => setIsEdit(true)}
+          >
+            Edit
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MyProfile;
