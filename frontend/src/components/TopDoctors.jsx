79d7649d3196dc3d493cafe7bf919d44d0f6934b
ritikdevelop <hrithikrana216@gmail.com>
2025-01-04 22:50:34 +0530

    Fix formatting and indentation in TopDoctors component for improved readability

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -17,7 +17,10 @@ const TopDoctors = () => {
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
         {doctors.slice(0, 10).map((item, index) => (
           <div
-            onClick={() => {navigate(`/appointment/${item._id}`); scrollTo(0,0)}}
+            onClick={() => {
+              navigate(`/appointment/${item._id}`);
+              scrollTo(0, 0);
+            }}
             key={index}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
           >
