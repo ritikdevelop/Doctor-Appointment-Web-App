0dfd372505e9173d647a97301484b2f5bd3ca53f
ritikdevelop <hrithikrana216@gmail.com>
2025-01-04 22:49:49 +0530

    Fix formatting in Navbar component for improved readability

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,10 @@ const Navbar = () => {
   const [token, setToken] = useState(true);
 
   return (
-    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+    <div
+      className="flex items-center justify-between 
+    text-sm py-4 mb-5 border-b border-b-gray-400"
+    >
       <img
         onClick={() => {
           navigate("/");
